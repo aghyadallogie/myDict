33486b5f892af7087c19a1bce6408f04a4c81a2c
Aghyad <zenlogie@gmail.com>
2023-02-23 16:43:29 +0100

    refactor login form input elements

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,40 +1,48 @@
-import { useRef, FormEvent } from "react";
+import { useState, FormEvent, ChangeEvent } from "react";
 import { styles } from "../../components/TranslateBox/TranslateBox.styles";
 import supabase from "../../config/supabaseClient";
 
 export const LandingPage = () => {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-
-  const email = emailRef?.current?.value || "";
-  const password = passwordRef?.current?.value || "";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // login action creator and reducer
 
   const handleLogin = async (event: FormEvent) => {
-      event.preventDefault();
-      console.log(password);
-      const { error, data } = await supabase.auth.signUp({ email, password });
+    event.preventDefault();
+    console.log('a) ', email, password);
+    const { error, data } = await supabase.auth.signUp({ email, password });
 
     if (error) {
       console.log("Registration error: ", error);
     }
 
-    console.log(data);
+    console.log('b) ', data);
   };
 
   return (
-    <div className="page" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <div
+      className="page"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <h2>Sign Up</h2>
       <div style={{ width: "80%", height: "100%", margin: "2rem auto 6rem" }}>
         <styles.StyledForm
           style={{ flexDirection: "column" }}
           onSubmit={handleLogin}
         >
-          <styles.Input type="email" ref={emailRef} placeholder="Enter Email" />
+          <styles.Input
+            type="email"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
+            placeholder="Enter Email"
+          />
           <styles.Input
             type="password"
-            ref={passwordRef}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
             placeholder="Enter Password"
           />
           <styles.Button style={{ margin: "0" }}>Register</styles.Button>
